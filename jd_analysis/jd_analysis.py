452685c6d5028bb0c598c0ba21bfd609873e67d7
lp <1402412786@qq.com>
2018-12-07 00:01:25 +0800

    处理数据，并画饼图

---
+++ jd_analysis/jd_analysis.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import os
+import json
+import re
+current_path = os.path.abspath(__file__)
+path = os.path.abspath(os.path.dirname(current_path) + os.path.sep + "..")
+
+def load_json():
+    """
+    加载数据
+    """
+    with open(path+'\\spider_allpage\\jd_list.json', 'r', encoding='utf-8') as f:
+        js_data =json.load(f)
+    # 计算种类,数量
+    dict_category = {}
+    for item in js_data:
+        if item['comm_name'] not in dict_category:
+            dict_category[item['comm_name']] = 1
+        dict_category[item['comm_name']] += 1
+    # 简化数据
+    new_dict = {}
+    for key, value in dict_category.items():
+        res = re.match(r'\S+', key).group()
+        if res not in new_dict:
+            new_dict[res] = value
+        new_dict[res] = new_dict[res] + value
+    print(new_dict)
+    # 更好的匹配
+    end_dict = {'协德': 0}
+    for key, value in new_dict.items():
+        res = re.match(r"\u91d1\u58eb\u987f|\u5341\u94e8|\u534f\u5fb7|\u4e09\u661f|\u91d1\u767e\u8fbe|\u5a01\u521a|\u82f1\u777f\u8fbe", key).group()
+        print(res)
+        if res not in new_dict:
+            end_dict[res] = value
+            print(end_dict)
+        end_dict[res] = end_dict[res] + value
+    print(end_dict)
+    return new_dict
+load_json()
+
+def image_pie(dict_goods):
+    """
+    画饼状图
+    :return:
+    """
+    goods_name = [i for i in dict_goods.keys()]
+    print(goods_name)
+    goods_value = [j for j in dict_goods.values()]
+    plt.figure(figsize=(20, 8), dpi=100)
+    plt.pie(goods_value, labels=goods_name, autopct="%1.2f%%")
+
+    plt.legend()
+    plt.title('内存条排行占比')
+    plt.show()
+
+
+# if __name__ == '__main__':
+    # dict_goods = load_json()
+    # image_pie(dict_goods)
+
--- spider_allpage/spider_page.py
+++ spider_allpage/spider_page.py
@@ -19,16 +19,23 @@ class SpiderJDPG:
             'Referer': 'https://search.jd.com/Search?keyword='+self.kw+'&enc=utf-8&wq=%E7%AC%94%E8%AE%B0%E6%9C%AC%E5%86%85%E5%AD%98%E6%9D%A1&pvid=a31e221f599440868f39b9794da8d579'
         }
         self.remaining = 'https://search.jd.com/s_new.php?keyword='+self.kw+'&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&psort=3&page={}&s=27&scrolling=y&log_id=1543829593.84658&tpl=1_M'
+        self.goods = []
 
     def send_request(self):
         response = requests.get(self.base_url, self.headers)
         data = response.content.decode('utf-8')
         return data
 
-    def save_json(self, data):
-        data_json = json.dumps(data)
-        with open('jd_list.json', 'a', encoding='utf-8') as f:
-            f.write(data_json+'\n')
+    def save_file(self, data):
+        # data_json = json.dumps(data)
+        # with open('jd_list.json', 'w', encoding='utf-8') as f:
+        #     f.write(data)
+        self.goods.append(data)
+
+    def save_json(self, goods):
+        with open('jd_list.json', 'w', encoding='utf-8') as f:
+            json.dump(goods, f, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False)
+        print('==============+保存成功+=================')
 
     def get_detailed(self, data):
         """
@@ -58,7 +65,7 @@ class SpiderJDPG:
                 'comm_img': comm_img,
                 'comm_intro': comm_intro
             }
-            self.save_json(temp)
+            self.save_file(temp)
 
     def get_detailedlater(self):
         """
@@ -73,6 +80,7 @@ class SpiderJDPG:
         for comm in li_list:
             comm_id = comm['data-sku']
             comm_name = comm.find('div', class_='p-name p-name-type-2').a.em.get_text()
+            print(comm_name)
             comm_price = '￥' + comm.find('div', class_='p-price').i.get_text()
             comm_img = comm.a.img['source-data-lazy-img']
             comm_intro = comm.find('div', class_='p-name p-name-type-2').a['title']
@@ -84,11 +92,12 @@ class SpiderJDPG:
                 'comm_img': comm_img,
                 'comm_intro': comm_intro
             }
-            self.save_json(temp)
+            self.save_file(temp)
 
     def run(self):
         i = 1
         while i <= self.page:
+            print('----------------', i)
             count = (i-1)*60+1
             self.base_url.format(i, count)
             self.remaining.format(i)
@@ -98,6 +107,8 @@ class SpiderJDPG:
             print('====================')
             i += 1
             time.sleep(1)
+        goods = self.goods
+        self.save_json(goods)
 
 
 if __name__ == '__main__':
